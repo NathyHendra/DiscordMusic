a68bb5d669d776b2415cdc1b06235055dd4dbe52
Ertassla <76439535+Ertassla@users.noreply.github.com>
2020-12-29 14:27:55 +0300

    Update Clear.js

--- commands/Clear.js
+++ commands/Clear.js
@@ -1,17 +1,9 @@
 const Discord = require('discord.js');
 const config = require("../settings/config.json");
 exports.run = function(client, message, args) {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have enough permissions!").then(m => m.delete(8000))
-if(!args[0]) return message.channel.send("Specify the number of messages.").then(m => m.delete(8000))
-message.channel.bulkDelete(args[0]).then(() => {
-  const embed = new Discord.MessageEmbed()
-       .setDescription(`Messages successfully deleted.`)
-  
-        .setFooter(`requested by ${message.author.username}`)
-        .setColor(config.cyan)
-        .setTimestamp()
-message.channel.send({ embed }).then(m => m.delete(8000))
-})
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have enough permissions!").then(z => z.delete({timeout: 8000}))
+  if(!args[0]) return message.reply("Specify the nubmer of messages").then(x => x.delete({timeout: 8000}));
+  message.channel.bulkDelete(args[0]).then(() => { message.channel.send(new Discord.MessageEmbed().setDescription("Messages successfully deleted.").setColor(config.cyan).setTimestamp().setFooter(`requested by ${message.author.username}`))})
 };
 
 exports.help = {
